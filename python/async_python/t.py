28ada6c515c188d3a770db1a915c51060a6e3fff
mangustik228 <bacek.mangust@gmail.com>
2023-10-11 01:09:04 +0700

    delete bulshit

--- python/async_python/t.py
+++
@@ -1,9 +0,0 @@
-result = [
-    'sensor_03_data: c8050eed40ef171cc2784d638b34fa781359331a3ed13a32e97cc31667ba',
-    'sensor_01_data: a043bae1603803a839c0cb52f511d9cb8f68d9d9b0341314a5534ea92b26',
-    'sensor_05_data: 3b36a45c17ff134eee52db9fa0d4af211849b3a47ed1640ca4b8601ee8d9',
-]
-
-
-result.sort(key=lambda r: int(r.split("_")[1]))
-print(result)
